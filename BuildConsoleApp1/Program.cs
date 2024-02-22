358ab650562c5595acccb63ea24ec0e3b11e6753
Staleo Poulose <stalleo@outlook.com>
2024-02-22 11:22:38 +0530

    made minor changes in program.cs

--- BuildConsoleApp1/Program.cs
+++ BuildConsoleApp1/Program.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace BuildConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string output = JsonConvert.SerializeObject(new object[] { });
         }
     }
 }
